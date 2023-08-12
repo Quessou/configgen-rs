95bb592282732ddb2a4dcbcdd12a9a4289ab67ba
Maxime <maximemikotajewski@gmail.com>
2023-08-12 16:50:33 +0200

    Added comparison for error values

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -15,3 +15,49 @@ pub enum Error {
     #[error("Writing failed")]
     WritingFailed(#[source] std::io::Error),
 }
+
+impl PartialEq for Error {
+    fn eq(&self, other: &Self) -> bool {
+        matches!(
+            (self, other),
+            (
+                Self::ConfigDirectoryAlreadyExists(_),
+                Self::ConfigDirectoryAlreadyExists(_)
+            ) | (
+                Self::ConfigFileAlreadyExists(_),
+                Self::ConfigFileAlreadyExists(_)
+            ) | (
+                Self::ConfigDirectoryCreationFailed(_),
+                Self::ConfigDirectoryCreationFailed(_)
+            ) | (Self::UnsupportedFormat(_), Self::UnsupportedFormat(_))
+                | (Self::SerializationFailed(_), Self::SerializationFailed(_))
+                | (Self::WritingFailed(_), Self::WritingFailed(_))
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::assert_ne;
+
+    use super::*;
+
+    #[test]
+    pub fn test_error_comparison() {
+        let err1 = Error::ConfigFileAlreadyExists(std::io::Error::new(
+            std::io::ErrorKind::AlreadyExists,
+            "",
+        ));
+        let err2 = Error::ConfigFileAlreadyExists(std::io::Error::new(
+            std::io::ErrorKind::AlreadyExists,
+            "",
+        ));
+        let err3 = Error::ConfigDirectoryCreationFailed(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "",
+        ));
+
+        assert_eq!(err1, err2);
+        assert_ne!(err1, err3);
+    }
+}
